50943a3ed1b31133972b84807e2c304b7bb43402
rakeshpandey <rakeshpandey791@gmail.com>
2020-01-10 12:18:30 +0530

    Updates

---
+++ precache-manifest.61c7784af1db497534c3b99c7186a2b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3ced6e18bb03e8b35172f977f88cad2",
+    "url": "/myscoot/index.html"
+  },
+  {
+    "revision": "8365d3988004a2cb44ed",
+    "url": "/myscoot/static/css/main.4b5e4dc2.chunk.css"
+  },
+  {
+    "revision": "6989dd9f834bcbd41e4c",
+    "url": "/myscoot/static/js/2.13b27747.chunk.js"
+  },
+  {
+    "revision": "8365d3988004a2cb44ed",
+    "url": "/myscoot/static/js/main.abc7c571.chunk.js"
+  },
+  {
+    "revision": "07debd966bd49481dbe4",
+    "url": "/myscoot/static/js/runtime-main.bb6ef83a.js"
+  }
+]);
